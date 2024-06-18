37acff8e2bc599e737b3f7501db75c573239b903
Artyom Veremeenko <arwer13@gmail.com>
2024-06-18 13:49:32 +0300

    chore: add pre-commit hooks and proper linter commands

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,32 +10,32 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("plugin:@typescript-eslint/recommended", "prettier"),
-    includeIgnoreFile(gitignorePath),
-{
+export default [
+  ...compat.extends("plugin:@typescript-eslint/recommended", "prettier"),
+  includeIgnoreFile(gitignorePath),
+  {
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
-
-    ignores: ["./dist"],
 
     rules: {
-        "@typescript-eslint/no-explicit-any": ["warn"],
-        "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/no-explicit-any": ["warn"],
+      "@typescript-eslint/no-unused-vars": ["warn"],
     },
-}, {
+  },
+  {
     files: ["./scripts/{**/,}*.js", "./test/{**/,}*.js"],
-
     languageOptions: {
-        globals: {
-            ...globals.mocha,
-        },
+      globals: {
+        ...globals.mocha,
+      },
     },
-}];
+  },
+];
