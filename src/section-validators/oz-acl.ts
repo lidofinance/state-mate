64a8d2e7e4a6ab5505351d0ab77ddabe7e2343ad
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2025-10-29 15:18:05 +0000

    feat: add oz acl checks

--- src/section-validators/oz-acl.ts
+++ src/section-validators/oz-acl.ts
@@ -1,20 +1,55 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { JsonRpcProvider } from "ethers";
+import { JsonRpcProvider, Interface, Contract } from "ethers";
 
 import { EntryField } from "src/common";
 import { logHeader2 } from "src/logger";
-import { ContractEntry, isTypeOfTB, ProxyContractEntryTB } from "src/typebox";
+import { ContractEntry, isTypeOfTB, ProxyContractEntryTB, StaticCallResult } from "src/typebox";
 
-import { SectionValidatorBase } from "./base";
+import { SectionValidatorBase, CheckLevel, needCheck } from "./base";
+
+const ACCESS_CONTROL_ABI = [
+  "function hasRole(bytes32 role, address account) view returns (bool)",
+  "function getRoleMemberCount(bytes32 role) view returns (uint256)",
+];
+
 export class OzAclSectionValidator extends SectionValidatorBase {
   constructor(provider: JsonRpcProvider, sectionName: EntryField = EntryField.ozAcl) {
     super(provider, sectionName);
   }
 
   override async validateSection(contractEntry: ContractEntry, contractAlias: string) {
+    if (!needCheck(CheckLevel.checksType, this.sectionName)) {
+      return;
+    }
+
     if (isTypeOfTB(contractEntry, ProxyContractEntryTB) && contractEntry.ozAcl) {
       logHeader2(this.sectionName);
-      //TODO
+
+      const address = contractEntry.address;
+      const iface = new Interface(ACCESS_CONTROL_ABI);
+      const contract = new Contract(address, iface, this.provider);
+
+      for (const [role, addrs] of Object.entries(contractEntry.ozAcl)) {
+        logHeader2(`Role: ${role}`);
+
+        // Check getRoleMemberCount(role) == addrs.length
+        const countCheck: StaticCallResult = {
+          signature: "getRoleMemberCount",
+          args: [role],
+          result: addrs.length,
+        };
+        await this._checkViewFunction(contract, "getRoleMemberCount", countCheck);
+
+        // Check hasRole(role, addr) == true for each addr
+        for (const addr of addrs) {
+          const hasRoleCheck: StaticCallResult = {
+            signature: "hasRole",
+            args: [role, addr],
+            result: true,
+          };
+          await this._checkViewFunction(contract, "hasRole", hasRoleCheck);
+        }
+      }
     }
   }
 }
